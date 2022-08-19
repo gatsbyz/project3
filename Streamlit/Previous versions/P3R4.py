7cd241271f43a8b53ec033724d02ce297cc11e03
TylerShubert <thinkcarbonx1@gmail.com>
2022-08-18 21:57:21 -0400

    Streamlit Frontend upload (Tyler 9:56PM)
    Streamlit frontend upload. **NEEDS BACKEND**

---
+++ Streamlit/Previous versions/P3R4.py
@@ -0,0 +1,65 @@
+from calendar import Calendar
+from click import option
+import streamlit as st
+
+
+
+import pandas as pd
+from pathlib import Path
+st.write("# Hi, this is Blue Skys first web app!:sunglasses:")
+st.write("")
+st.write("# EtherCAR RENTAL CAR!")
+st.write("By BlueSky")
+
+titleIMG = ("Title.PNG")
+st.image(titleIMG)
+
+
+
+#Price Chart import
+Price1Chart = pd.read_csv(
+    Path("PriceChart1.csv"),
+    infer_datetime_format=False)
+PriceChartDF = pd.DataFrame(Price1Chart)
+
+
+
+
+#Image Data
+Mid_Size = ("mid.jpg")
+lux = ("lux.jpg")
+MiniVan = ("Mini.jpg")
+Comp = ("COMP.jpg")
+
+
+#Car selection
+st.write("# Pick your Car!")
+
+if st.button("Mid-Sized!"):
+     st.image(Mid_Size)
+
+if st.button("Compact Car!"):
+    st.image(Comp)
+
+if st.button("Luxary Car!"):
+    st.image(lux)
+
+if st.button("MiniVan"):
+    st.image(MiniVan)
+
+
+#Sidebar selection
+
+#Hours or days sidebar selection
+H_R = ["Hours", "Days"]
+hours_days = st.sidebar.selectbox("Hours or Days?", H_R)
+
+
+#Number of Hours or Days sidebar selection
+number_of_H_R = st.sidebar.number_input("Number of Hours/Days?")
+
+#Day of Rental selection
+Calendar_input = st.sidebar.date_input("Please select your rental date: ",value=None, min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+
+
+
